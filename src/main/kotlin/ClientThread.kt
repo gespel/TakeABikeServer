72a830e774c02c1c882e7bc117eda1b5b3154706
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2022-09-28 14:56:41 +0200

    gson

--- src/main/kotlin/ClientThread.kt
+++ src/main/kotlin/ClientThread.kt
@@ -1,12 +1,34 @@
+import com.google.gson.Gson
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.Socket
+import java.sql.DriverManager
 
 class ClientThread(private val client: Socket): Thread() {
     public override fun run() {
         println("Client connected : ${client.inetAddress.hostAddress}")
-        println(BufferedReader(InputStreamReader(client.inputStream)).readLine())
-        client.getOutputStream().write("Bla".toByteArray())
+        val fromClient = BufferedReader(InputStreamReader(client.inputStream)).readLine()
+        println(fromClient)
+        //client.getOutputStream().write("Bla".toByteArray())
+        var gson = Gson()
+        var model = gson.fromJson(fromClient)
         client.close()
+
+        /*try {
+            Class.forName("com.mysql.jdbc.Driver")
+            val con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/sonoo", "root", "root"
+            )
+            //here sonoo is database name, root is username and password
+            val stmt = con.createStatement()
+            val rs = stmt.executeQuery("select * from emp")
+            while (rs.next()) println(rs.getInt(1).toString() + "  " + rs.getString(2) + "  " + rs.getString(3))
+            con.close()
+        } catch (e: Exception) {
+            println(e)
+        }*/
     }
 }
+
+
+
